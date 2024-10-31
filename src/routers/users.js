556973a98559a16b3c9367340edbd25a6a313936
Yuri <wall.mart.boss@proton.me>
2024-10-31 20:30:05 +0200

    fix after 2 PR

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,27 +1,27 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { updateUserSchema } from '../validation/users.js';
-import { getCurrentUserController, updateThemeController, updateUserController } from '../controllers/users.js';
+import {
+  getCurrentUserController,
+  updateThemeController,
+  updateUserController,
+} from '../controllers/users.js';
 import { authenticate } from '../middlewares/authenticate.js';
-import { upload } from "../middlewares/multer.js";
+import { upload } from '../middlewares/multer.js';
 import { validateBody } from '../middlewares/validateBody.js';
 
 const router = Router();
 
-router.get(
-    '/current',
-    authenticate,
-    ctrlWrapper(getCurrentUserController)
+router.get('/current', authenticate, ctrlWrapper(getCurrentUserController));
+
+router.patch('/theme', authenticate, ctrlWrapper(updateThemeController));
+
+router.patch(
+  '/info',
+  authenticate,
+  upload.single('avatar'),
+  validateBody(updateUserSchema),
+  ctrlWrapper(updateUserController),
 );
-
-router.patch('/themes',
-    authenticate,
-    ctrlWrapper(updateThemeController));
-
-router.patch('/update',
-    authenticate,
-    upload.single('avatar'),
-    validateBody(updateUserSchema),
-    ctrlWrapper(updateUserController));
 
 export default router;
