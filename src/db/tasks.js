5dbdf58a676fc5d51636d7e6b522001a78f80d96
d1zzyroma <nataliawologin@gmail.com>
2024-11-01 12:57:51 +0100

    312

--- src/db/tasks.js
+++ src/db/tasks.js
@@ -1,13 +1,29 @@
 import mongoose from 'mongoose';
 
 const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  color: { type: String, enum: ['blue', 'pink', 'green', 'gray'], required: true },
-  date: { type: Date, required: false },
-  boardId: { type: mongoose.Schema.Types.ObjectId, ref: 'Board', required: true },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
+  title: { 
+    type: String, required: true 
+  },
+  description: { 
+    type: String, required: true 
+  },
+  color: { 
+    type: String, 
+    enum: ['blue', 'pink', 'green', 'gray'], 
+    required: true 
+  },
+  date: { 
+    type: Date,
+    required: false 
+  },
+  boardId: { 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Board', required: true 
+  },
+},
+{
+  timestamps: true,
+  versionKey: false
 });
 
 export const Task = mongoose.model('Task', taskSchema);
