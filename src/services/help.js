01c4e541963fbefb1f64d5310ed939b427b67dd2
Yuri <wall.mart.boss@proton.me>
2024-11-05 03:10:51 +0200

    change mail to my

--- src/services/help.js
+++ src/services/help.js
@@ -5,7 +5,7 @@ import { sendEmail } from '../utils/sendMail.js';
 export const requestNeedHelp = async (email, message) => {
   await sendEmail({
     from: env(SMTP.SMTP_FROM),
-    to: 'taskpro.project@gmail.com',
+    to: 'yuriwork26@gmail.com',
     subject: 'HELP MESSAGE',
     text: `Message from ${email}: '${message}'`,
   });
