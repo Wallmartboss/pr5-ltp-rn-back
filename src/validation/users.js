dc14b2de2d35723e91be2e31d39a7e0bc3b2ebb8
Yuri <wall.mart.boss@proton.me>
2024-11-06 23:45:47 +0200

    upd user validation

--- src/validation/users.js
+++ src/validation/users.js
@@ -1,8 +1,9 @@
 import Joi from 'joi';
 
 export const updateUserSchema = Joi.object({
-    name: Joi.string().min(3).max(20),
-    email: Joi.string().email().min(3).max(20),
-    theme: Joi.string().valid('light', 'dark', 'violet'),
-    avatar: Joi.string(),
+  name: Joi.string().min(3).max(20),
+  email: Joi.string().email().min(3).max(20),
+  theme: Joi.string().valid('light', 'dark', 'violet'),
+  avatar: Joi.string(),
+  password: Joi.string().min(8),
 });
