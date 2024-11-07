5ab7d9f803e950912f8c86e3af0aea5efcc55788
Yuri <wall.mart.boss@proton.me>
2024-11-07 20:04:51 +0200

    upd2 isValidId

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,9 +1,9 @@
 import { isValidObjectId } from 'mongoose';
 import createError from 'http-errors';
 
-export const isValidId = (param) => asyc (req, res, next) => {
-  const id  = req.params[param];
-  if (!isValidObjectId(Id)) {
+export const isValidId = (param) => (req, res, next) => {
+  const id = req.params[param];
+  if (!isValidObjectId(id)) {
     const error = createError(
       400,
       `Invalid contact ID: ${param}. It must be a valid MongoDB ObjectId.`,
