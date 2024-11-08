c6b5370c0cc36fa8d95c02c462d6e865f30ac4ba
Yuri <wall.mart.boss@proton.me>
2024-11-08 14:14:31 +0200

    upd errorhandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,23 +1,18 @@
 import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || 'Server error';
-  res.status(status).json({ message });
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
+  });
 };
-
-//   if (err instanceof HttpError) {
-//     res.status(err.status).json({
-//       status: err.status,
-//       message: err.name,
-//       data: err,
-//     });
-//     return;
-//   }
-
-//   res.status(500).json({
-//     status: 500,
-//     message: 'Something went wrong',
-//     data: err.message,
-//   });
-// };
