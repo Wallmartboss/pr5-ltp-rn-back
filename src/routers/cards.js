a2c6285128659a436e74bcfd80176ac18b045142
Yuri <wall.mart.boss@proton.me>
2024-11-08 02:24:02 +0200

    upd cards router

--- src/routers/cards.js
+++ src/routers/cards.js
@@ -14,10 +14,15 @@ import { cardSchema, updateCardSchema } from '../validation/cards.js';
 const cardsRouter = Router();
 
 cardsRouter.get('/', getAllCardsController);
-cardsRouter.get('/:cardId', isValidId, getCardByIdController);
+cardsRouter.get('/:cardId', isValidId('cardId'), getCardByIdController);
 cardsRouter.post('/', validateBody(cardSchema), createCardController);
-cardsRouter.patch('/:cardId', isValidId, validateBody(updateCardSchema), updateCardController);
-cardsRouter.delete('/:cardId', isValidId, deleteCardController);
+cardsRouter.patch(
+  '/:cardId',
+  isValidId('cardId'),
+  validateBody(updateCardSchema),
+  updateCardController,
+);
+cardsRouter.delete('/:cardId', isValidId('cardId'), deleteCardController);
 cardsRouter.patch('/move/:cardId', moveCardController);
 
 export default cardsRouter;
